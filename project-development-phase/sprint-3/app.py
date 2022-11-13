fa95f2966bcc79cff462448b65e1008b9c3dfc6b
Ameenul Marsook <79187744+Ameenul557@users.noreply.github.com>
2022-11-13 21:17:45 +0530

    Add files via upload

--- project-development-phase/sprint-3/app.py
+++ project-development-phase/sprint-3/app.py
@@ -1,9 +1,3 @@
-
-# Sprint 3
-# Flask Integration
-
-
-
 import pandas as pd
 import numpy as np
 from flask import Flask, render_template, Response, request
@@ -13,19 +7,17 @@ from sklearn.preprocessing import LabelEncoder
 
 app = Flask(__name__)
 
-#route to home
 @app.route('/')
 def index():
     return render_template('index.html')
 
-#route to prediction form page
 @app.route('/predict')
 def predict():
     return render_template('prediction.html')
 
 #method to predict from deployed model
 def predictFromDeploymentModel(userInput):
-    API_KEY = "API_KEY_FROM_IBM"
+    API_KEY = ""
     token_response = requests.post('https://iam.cloud.ibm.com/identity/token', data={"apikey":
     API_KEY, "grant_type": 'urn:ibm:params:oauth:grant-type:apikey'})
     mltoken = token_response.json()["access_token"]
@@ -34,16 +26,33 @@ def predictFromDeploymentModel(userInput):
     response_scoring = requests.post('https://us-south.ml.cloud.ibm.com/ml/v4/deployments/9528ebfb-f57a-4b7b-9684-d745eea3da24/predictions?version=2022-11-08', json=payload_scoring,
     headers={'Authorization': 'Bearer ' + mltoken})
     predictions = response_scoring.json()
-    print(predictions['predictions'][0]['values'][0][0])
+    return (predictions['predictions'][0]['values'][0][0])
 
+def getMonthNumber(monthName):
+    months = {
+        'january' : 1,
+        'february' : 2,
+        'march' : 3,
+        'april' : 4,
+        'may' : 5,
+        'june' : 6,
+        'july' : 7,
+        'august' : 8,
+        'september' : 9,
+        'october' : 10,
+        'november' : 11,
+        'december' : 12,
+    }
 
-#Flask integration
+    return months[monthName]
+    
+
 @app.route('/y_predict', methods=['GET', 'POST']) 
 def y_predict():
     regyear = int(request.form['regyear'])
     powerps = float(request.form['powerps'])
     kms = float(request.form['kms'])
-    regmonth = int(request.form.get('regmonth'))
+    regmonth = getMonthNumber(request.form.get('regmonth'))
     gearbox = request.form['gearbox'] 
     damage = request.form['dam']
     model = request.form.get('modeltype')
@@ -54,6 +63,7 @@ def y_predict():
     new_row = {'yearOfRegistration':regyear, 'powerPS':powerps, 'kilometer': kms, 'monthOfRegistration':regmonth, 'gearbox': gearbox, 'notRepairedDamage': damage,'model':model, 'brand': brand, 'fuelType': fuelType, 'vehicleType': vehicletype}
     new_df = pd.DataFrame(columns =[ 'vehicleType', 'yearOfRegistration', 'gearbox', 'powerPS', 'model', 'kilometer', 'monthOfRegistration', 'fuelType','brand', 'notRepairedDamage']) 
     new_df = new_df.append(new_row, ignore_index = True)
+
 
     labels = ['gearbox', 'notRepairedDamage', 'model', 'brand', 'fuelType', 'vehicleType']
 
@@ -67,7 +77,10 @@ def y_predict():
 
     labeled = new_df[['yearOfRegistration','powerPS','kilometer','monthOfRegistration']+[x+'_labels' for x in labels]]
     X = labeled.values
+    print(X)
+
     predictFromDeploymentModel(list(X[0]))
+    
 
     return render_template('prediction.html')
 
